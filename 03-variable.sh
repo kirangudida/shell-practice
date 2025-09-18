a4a785ac451c7fe35f66d8356a2435b1de173f91
kiran <kgudida@gmail.com>
2025-09-18 21:57:32 +0800

    variables test

---
+++ 03-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+x=ammulu
+y=jayasree
+echo "$x:: Hi $y, how are you?"  
+echo "$y:: I'm good, thanks! How about you?"
+echo "$x:: I'm doing well too. What have you been up to lately?"  
+echo "$y:: Just working on some projects and spending time with family. You?"
