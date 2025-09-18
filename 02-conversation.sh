3ea9168557690a84179c0499212ac565a45ffa8e
kiran <kgudida@gmail.com>
2025-09-18 21:48:50 +0800

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Kiran:: Hi Souji, how are you?"  
+echo "Souji:: I'm good, thanks! How about you?"
+echo "Kiran:: I'm doing well too. What have you been up to lately?"  
+echo "Souji:: Just working on some projects and spending time with family. You?"
